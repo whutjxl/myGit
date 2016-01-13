949f4ca78a290e87d1af020a80270e84ee3fbb81
jxl <whutjxl@example.com>
2016-01-13 14:15:34 +0800

    addChineseHello

--- hello.java
+++ hello.java
@@ -1,10 +1,18 @@
 public class hello{
     public static void main(String[] args){
-        sayHello("Hello Java World!",60);
+        sayHello("Hello Java World!",10);
+        System.out.println("-------------------------------------");
+        sayChineseHello("你好！",6);
     }
     public static void sayHello(String s,int n){
         for(int i=0;i<n;++i){
             System.out.println(s+"--- "+i);
+        }
+    }
+
+    public static void sayChineseHello(String s,int n){
+        for(int i=0;i<10;++i){
+            System.out.println(s+"---->"+i);
         }
     }
 }
