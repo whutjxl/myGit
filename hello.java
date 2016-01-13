3a49fd313e3fbfdbdc45af1cdbe4cb1e87d44ce3
jxl <whutjxl@example.com>
2016-01-13 14:38:35 +0800

    add say!

--- hello.java
+++ hello.java
@@ -3,6 +3,8 @@ public class hello{
         sayHello("Hello Java World!",10);
         System.out.println("-------------------------------------");
         sayChineseHello("你好！",6);
+        System.out.println("-------------------------------------");
+        say();
     }
     public static void sayHello(String s,int n){
         for(int i=0;i<n;++i){
@@ -13,6 +15,11 @@ public class hello{
     public static void sayChineseHello(String s,int n){
         for(int i=0;i<10;++i){
             System.out.println(s+"---->"+i);
+        }
+    }
+    public static void say(){
+        for(int i=0;i<10;++i){
+            System.out.println("say!");
         }
     }
 }
